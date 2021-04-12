72811ce6adb5c60953ed4df670f574d54c8834f0
jviatge <julien.viatge@gmail.com>
2021-04-12 23:16:18 +0200

    wip

--- test.py
+++
@@ -1,9 +0,0 @@
-table = "boujour_tois"
-table = ''.join(word.title() for word in table.split())
-CamelCase = table.replace('_','')[:-1]
-label = table.replace('_',' ')[:-1]
-labelSingu = table.replace('_',' ')
-
-print(CamelCase)
-print(label)
-print(labelSingu)
